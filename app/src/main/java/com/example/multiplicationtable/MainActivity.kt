15d59e4f1e533ec442451dcdf6beb0fb94a9b739
gretakasongo <gretakasongo07@gmail.com>
2026-04-05 13:15:51 +0100

    Design UI

---
+++ app/src/main/java/com/example/multiplicationtable/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.multiplicationtable
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.multiplyapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val multiplybtn = findViewById<Button>(R.id.multiplybtn)
+        val numInput = findViewById<EditText>(R.id.numInput)
+        multiplybtn.setOnClickListener {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,4 +24,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "Multiply app"
 include(":app")
+include(":app")
+include(":app")
  
