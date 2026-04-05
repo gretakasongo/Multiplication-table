d12f7bfc4426d58dd2718dfec902f66818da54a8
gretakasongo <gretakasongo07@gmail.com>
2026-04-05 14:39:09 +0100

    Data on table display

--- app/src/main/java/com/example/multiplicationtable/TableDisplay.kt.kt
+++ app/src/main/java/com/example/multiplicationtable/TableDisplay.kt.kt
@@ -1,27 +1,29 @@
 package com.example.multiplicationtable
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat.enableEdgeToEdge
 import androidx.core.view.WindowInsetsCompat
 import com.example.multiplyapp.R
 
-class TableDisplay {
-    companion object {
-        val kt: Any
-    }
-}.kt : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_table)
+        // assigning data from intent to a variable
+        val bundle: Bundle? = intent.extras
+        // getting the table number from the intent
+        val tableString: String? = bundle!!.getString("tableNumber")
+        // converting the table number to an integer
+        val tableNumber = tableString!!.toInt()
+        val Table = findViewById<TextView>(R.id.Table)
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
-}
+            insets}}}
