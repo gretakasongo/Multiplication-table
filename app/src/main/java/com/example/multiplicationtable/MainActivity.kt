7bba846d585d1ac700a104f8c43e5390af6e1a56
gretakasongo <gretakasongo07@gmail.com>
2026-04-05 13:21:15 +0100

    added comments and variables

--- app/src/main/java/com/example/multiplicationtable/MainActivity.kt
+++ app/src/main/java/com/example/multiplicationtable/MainActivity.kt
@@ -14,9 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        // Created variables for the buttons and the input
         val multiplybtn = findViewById<Button>(R.id.multiplybtn)
         val numInput = findViewById<EditText>(R.id.numInput)
+        // Created an on click listener for multiply button
         multiplybtn.setOnClickListener {
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
